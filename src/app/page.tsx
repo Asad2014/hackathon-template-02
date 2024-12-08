d3d9ee17a1eef0cc2af6011cde141549c0dff8c4
asad2014 <asadkk2014@gmail.com>
2024-12-09 01:23:30 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Hero from './components/Hero'
+import Brand from './components/Brand'
+import Newproducts from './components/Newproducts'
+import { TbDeviceIpadHorizontalExclamation } from 'react-icons/tb'
+import Popularproducts from './components/Popularproducts'
+import Information from './components/Information'
+import Getintouch from './components/Getintouch'
+import Productlisting from './Productlisting/page'
+import About from './About/page'
+
 
 const page = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <Hero />
+      <Brand />
+      <Newproducts />
+      <Popularproducts />
+      <Information />
+      <Getintouch />
+      <Productlisting />
+      <About />  
     </div>
   )
 }
